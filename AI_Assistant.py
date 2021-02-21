04c21a897903eabe9e74e49e5874803bf87f05b8
anasagag1997@gmail.com <Abughazaleh.1997>
2021-02-21 15:04:40 +0200

    added a test comment

--- AI_Assistant.py
+++ AI_Assistant.py
@@ -25,12 +25,13 @@ import subprocess
 ###########
 # Just testing git
 ###########
+
 # _________________________________________________________________
 # Setup voice engine
 
 engine = pyttsx3.init(driverName='sapi5', debug=True)  # initialize the text-to-speech engine
 voices = engine.getProperty('voices')  # get available voices
-engine.setProperty('voice', 'voices[1].id')  # set male voice (number 1)
+engine.setProperty('voice', voices[0].id)  # set male voice (number 0), female voice (number 2)
 engine.setProperty('rate', 175)  # set speech speed/rate
 
 
@@ -44,7 +45,7 @@ def speak(text):
 
 
 def greeting():
-    hello_list = ["Hello", "Hi", "Haalaa"]
+    hello_list = ["Hello", "Hi"]
     greeting_list = ["Good Morning", "Good Afternoon", "Good Evening"]
     hour = datetime.datetime.now().hour
 
@@ -65,8 +66,10 @@ def greeting():
 def takeCommand():
     r = sr.Recognizer()
     repeat_command = ["Pardon me, please say that again", "Sorry, could you repeat that", "Could you repeat that"]
-    now = datetime.datetime.now().time()  # time object
-    print(now)
+
+    # now = datetime.datetime.now().time()  # time object delete
+    # print(now)  # delete
+
     with sr.Microphone() as source:
         print("Listening...")
         audio = r.listen(source)
@@ -78,17 +81,27 @@ def takeCommand():
         except Exception as e:
             speak(random.choice(repeat_command))
             return "None"
+
         return statement
 
 
 def digital_commands(audio):
-    global ai_assistant_running
+    global ai_assistant_running, silence_counter
+
     statement = audio
-    created_by = ["who made you", "who created you", "who discovered you"]
+    created_by = ["who made you", "who created you", "who discovered you", "your name"]
     bye_bye = ["bye", "stop", "shut down", "shutdown", "turn off", "turnoff"]
+    available_commands = ["commands", "tasks", "assist"]
+    connectors = ["how", "what", "can", "do"]
+
+    print("Init statement", statement)
+    print("Init statement", type(statement))
+    print("silence_counter", silence_counter)
+    print("silence_counter", type(silence_counter))
 
     # todo: add again function: repeats the last answer
-    # todo: add sissers rock paper game
+    # todo: add scissors rock paper game
+    # todo: tell a joke
 
     if any(sub_list in statement for sub_list in bye_bye):
         speak('your personal assistant Kareem is shutting down')
@@ -142,33 +155,61 @@ def digital_commands(audio):
         speak("I am Kareem, a personal assistant to help you in your tasks.")
 
     elif any(sub_list in statement for sub_list in created_by):
-        speak("I was built by Anas")
-        print("I was built by Anas")
+        speak("My name is Kareem. I was built by Anas.")
+        print("My name is Kareem. I was built by Anas.")
+
+    elif any(sub_list in statement for sub_list in available_commands) and \
+        any(sub_list in statement for sub_list in connectors):
+        speak("I can do a couple of tasks, such as: ... searching online, telling the time, search Wikipedia")
+        print("I can do a couple of tasks, such as: ... searching online, telling the time, search Wikipedia")
+
+    elif statement == "none":
+        silence_counter += 1
+        # speak("Did you say anything?, I did not hear that.")
+        # print("statement", statement)
+        # print("statement type", type(statement))
+
+    elif type(statement) is str and statement != "none":  # and "None" in statement:
+        speak("Unfortunately, I can't do this command yet.")
+        speak("You can check the available commands mentioned in the documentation.")
+
+    if silence_counter > 2:
+        time.sleep(3)
+        speak("Call me back when you need me.")
+        engine.stop()
+        ai_assistant_running = False
 
 
 if __name__ == '__main__':
 
-    global ai_assistant_running
-
+    # global ai_assistant_running
+    silence_counter = 0
     Loading_assistant_speech = "Loading personal assistant Kareem"
-    speech = ["Tell me how can I help you?", "How can I help you?", "Can I help you?", "Hey, how can I assist you?"]
+    speech = ["Tell me how can I help you?", "How can I help you?", "Can I help you?", "How can I assist you?"]
 
     print(Loading_assistant_speech)
+    # time.sleep(3)
+    print("After 1")
     speak(Loading_assistant_speech)
+    # time.sleep(3)
+    print("After 2")
     greeting()
-
+    # time.sleep(3)
+    print("After 3")
     # count = 1
     ai_assistant_running = True
     if ai_assistant_running: speak(random.choice(speech))
-
+    print("After 4")
     # time_intervals = []
     while ai_assistant_running:
+        # engine.startLoop()
         # now = datetime.datetime.now().time() # time object
         # print(now)
-        # time_intervals.append(now)
+        # time_intervals.append(now)Loading personal assistant Kareem
         audio = takeCommand().lower()  # audio is type: str
+        print("After 5")
         digital_commands(audio)
-
+        print("After 6")
 # ___________________________________________________________________________________________________________
 # Improvements:
 
