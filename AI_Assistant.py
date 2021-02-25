de5da411c35dd7a9403faafc3fc9fccc63551a33
anasagag1997@gmail.com <Abughazaleh.1997>
2021-02-25 14:48:43 +0200

    Modify the app GUI, enhance the code a bit, add some functions and commands

--- AI_Assistant.py
+++ AI_Assistant.py
@@ -5,8 +5,6 @@
 import speech_recognition as sr
 import pyttsx3
 
-# import gui_interface as gui
-
 # libs for user commands
 import datetime
 import wikipedia
@@ -16,13 +14,6 @@ from ecapture import ecapture as ec
 import pyjokes
 import random
 import config
-# not used
-# import json
-# import requests
-# import pyaudio
-# import wolframalpha
-# import os
-# import subprocess
 
 from tkinter import *
 from tkinter.ttk import *
@@ -35,25 +26,17 @@ class App(threading.Thread):
         threading.Thread.__init__(self)
         self.start()
         self.root = None
+        self.button = None
         self.txt = None
         self.assistant_mode = None
 
     def configure_root(self):
-
         self.root.title("Personal Assistant GUI")
-        self.root.rowconfigure(0, minsize=100, weight=1)
-        self.root.columnconfigure(1, minsize=100, weight=1)
+        self.root.rowconfigure(0)  # , minsize=100, weight=1
+        self.root.columnconfigure(0)  # , minsize=100, weight=1
         # self.root.geometry("")
         self.root.iconbitmap(r"images\microphone.png")
         # self.root.resizable(width=FALSE, height=FALSE)
-
-    def create_widgets(self):
-        Label(self.root, text='Personal Assistant', anchor=CENTER).grid(row=0, columnspan=2)
-        Button(self.root, text='Start Recording', cursor='hand2').grid(row=1, column=0,
-                                                                       sticky=W + E)  # , image=mic_image
-        self.txt = Text(self.root)
-        self.txt.grid(row=1, column=1, sticky=W + E, padx=20, pady=20)
-        print('App create widgets')
 
     def button_value(self):
         self.assistant_mode = 1
@@ -66,11 +49,13 @@ class App(threading.Thread):
     def run(self):
         self.root = Tk()
         self.configure_root()
-        Label(self.root, text='Personal Assistant', anchor=CENTER).grid(row=0, columnspan=2)
-        Button(self.root, text='Start Assistant', command = self.button_value,cursor='hand2').grid(row=1, column=0,
-                                                                       sticky=W + E)  # , image=mic_image
-        self.txt = Text(self.root)
-        self.txt.grid(row=1, column=1, sticky=W + E, padx=20, pady=20)
+        Label(self.root, text='Personal Assistant', anchor=CENTER,
+              font=("Poor Richard", 30, "bold")).grid(row=0, padx=10, pady=5)
+        self.button = Button(self.root, text='Start Assistant', command=self.button_value,
+                             cursor='hand2', state=DISABLED)
+        # self.button.grid(row=1, column=0, padx=5, pady=5)
+        self.txt = Text(self.root, wrap=WORD, state=DISABLED, spacing1=5)
+        self.txt.grid(row=2, column=0, sticky=W + E, padx=10, pady=10)
         self.root.update()
         self.root.update_idletasks()
 
@@ -82,10 +67,22 @@ class App(threading.Thread):
         self.root.mainloop()
 
     def text_insert_assistant(self, text_input):
+        self.txt.configure(state=NORMAL)
         self.txt.insert(END, "Assistant: " + text_input + "\n")
+        self.txt.configure(state=DISABLED)
 
     def text_insert_user(self, text_input):
+        self.txt.configure(state=NORMAL)
         self.txt.insert(END, "User: " + text_input + "\n")
+        self.txt.configure(state=DISABLED)
+
+    def restart_personal_assistant(self):
+        if config.ai_assistant_running == False and app.assistant_mode == 0:
+            self.button.config(state=NORMAL)
+            self.button.grid(row=1, column=0, padx=5, pady=5)
+        else:
+            self.button.config(state=DISABLED)
+            self.button.grid_forget()
 
 
 # initialize the GUI class, to be able to use its functions in the code
@@ -113,19 +110,19 @@ def greeting():
 
     if 0 <= hour < 12:
         speech = random.choice(hello_list) + " " + greeting_list[0]
-        speak(speech)
         app.text_insert_assistant(speech)
         print(speech)
+        speak(speech)
     elif 12 <= hour < 18:
         speech = random.choice(hello_list) + " " + greeting_list[1]
-        speak(speech)
         app.text_insert_assistant(speech)
         print(speech)
+        speak(speech)
     else:
         speech = random.choice(hello_list) + " " + greeting_list[2]
-        speak(speech)
         app.text_insert_assistant(speech)
         print(speech)
+        speak(speech)
 
 
 def takeCommand():
@@ -144,7 +141,9 @@ def takeCommand():
             print(f"User said:{statement}\n")
 
         except Exception:
-            speak(random.choice(repeat_command))
+            speech = random.choice(repeat_command)
+            app.text_insert_assistant(speech)
+            speak(speech)
             return "None"
 
         return statement
@@ -157,25 +156,23 @@ def digital_commands(audio):
     bye_bye = ["bye", "stop", "shut down", "shutdown", "turn off", "turnoff"]
     available_commands = ["commands", "tasks", "assist"]
     connectors = ["how", "what", "can", "do"]
-
-    # print("Init statement", statement)
-    # print("Init statement", type(statement))
-    # print("silence_counter", silence_counter)
-    # print("silence_counter", type(silence_counter))
+    personal_mood = ["I'm great, thanks.", "I'm doing good, thanks.", "Feeling fantastic!"]
 
     # todo: add scissors rock paper game
 
     if any(sub_list in statement for sub_list in bye_bye):
         speak('your personal assistant Kareem is shutting down')
         time.sleep(0.2)
-        speak('Good bye')
         app.text_insert_assistant('your personal assistant Kareem is shutting down, Good bye')
+        speak('Good bye')
         print('your personal assistant Kareem is shutting down, Good bye')
+        # configuring for shutting down the personal assistant
         engine.stop()
         config.ai_assistant_running = False
         config.silence_counter = 0
         app.assistant_mode = 0
         config.was_assistant_off = 1
+        app.restart_personal_assistant()
         # to exit the function and not check the other commands
         return None
 
@@ -190,8 +187,8 @@ def digital_commands(audio):
         statement = statement.replace("wikipedia", "")
         results = wikipedia.summary(statement, sentences=3)
 
-        speak("According to Wikipedia")
         app.text_insert_assistant("According to Wikipedia" + results)
+        speak("According to Wikipedia")
         print(results)
         speak(results)
 
@@ -234,6 +231,11 @@ def digital_commands(audio):
         speak("Your search window is ready!")
         time.sleep(5)
 
+    elif 'how are you' in statement:
+        speech = random.choice(personal_mood)
+        app.text_insert_assistant(speech)
+        speak(speech)
+
     elif 'who are you' in statement or 'what can you do' in statement:
         app.text_insert_assistant("I am Kareem, a personal assistant to help you in your tasks.")
         speak("I am Kareem, a personal assistant to help you in your tasks.")
@@ -245,16 +247,16 @@ def digital_commands(audio):
 
     elif any(sub_list in statement for sub_list in created_by):
         app.text_insert_assistant("My name is Kareem. I was built by Anas.")
-        speak("My name is Kareem. I was built by Anas.")
         print("My name is Kareem. I was built by Anas.")
+        speak("My name is Kareem. I was built by Anas.")
 
     elif any(sub_list in statement for sub_list in available_commands) and \
-        any(sub_list in statement for sub_list in connectors):
+            any(sub_list in statement for sub_list in connectors):
         text = "I can do a couple of tasks, such as: ... searching online, telling the time, " \
                "search Wikipedia, telling jokes too"
         app.text_insert_assistant(text)
-        speak(text)
         print(text)
+        speak(text)
 
     elif type(statement) is str and statement != "none":  # and "None" in statement:
         app.text_insert_assistant("Unfortunately, I can't do this command yet.")
@@ -274,6 +276,7 @@ def turn_assistant_on():
     global engine
     engine = pyttsx3.init(driverName='sapi5', debug=True)  # initialize the text-to-speech engine
     config.ai_assistant_running = True
+    app.text_insert_assistant("\n Personal Assistant in On!\n")
 
 
 if __name__ == '__main__':
@@ -287,18 +290,12 @@ if __name__ == '__main__':
 
     # run greeting function
     greeting()
-    # set ai_assistant_running True to run the while loop below
-    # config.ai_assistant_running = True
     if config.ai_assistant_running: speak(random.choice(speech))
 
     while True:
 
         if config.ai_assistant_running:
-            # if config.was_assistant_off:
-            #     greeting()
-            #     config.was_assistant_off = 1
-            # hear the user and take command
-            print("still in while loop ...")
+            app.restart_personal_assistant()
             audio = takeCommand().lower()
             # take action and do what the user asks for
             digital_commands(audio)
@@ -314,7 +311,13 @@ if __name__ == '__main__':
 # 5. When the assistant is not capable of doing a command, it should tell the user so
 # (meaning the program does not have that command listed)
 # 7. Turn assistant off after some rounds when no reply is received
+# shift print before speak command
+# Text Widget not editable
+# modify "start recording" button
+
 # Dev:
-# 3. Better user sentence analyses, to have more robust understanding of the command
-# 4. Deploy as a web app
 # 6. Test each command function
+# remove the print statements
+# auto scroll text widget till end
+# fix error when exiting the app
+# add how r u command
